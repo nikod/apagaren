894599095291ea5802c00ff073ef190102fdf2cf
Nicolás Delgado <nicolasdelgado@gmail.com>
2011-09-27 01:20:37 -0300

    Agregadas las conversiones a segundos y algunos cambios en el codigo

--- Apagar.py
+++ Apagar.py
@@ -25,13 +25,38 @@ class gui:
 		
 		self.Ventana.show_all()
 		
-	def Apagar(self):
-		bus = dbus.SystemBus()
-		bus_object = bus.get_object("org.freedesktop.ConsoleKit", "/org/freedesktop/ConsoleKit/Manager")
-		bus_object.Stop(dbus_interface="org.freedesktop.ConsoleKit.Manager")
+	def Apagar(self, *args):
+		Dbus_Apagar()
+		gtk_main_quit(*args)
 	
 	def gtk_main_quit (self, *args):
 		gtk.main_quit()
+
+	def cb_activo (self, combobox):
+		model = self.combobox.get_model()
+		activo = self.combobox.get_active()
+		return model[active][0]
+      		 
+def Dbus_Apagar():
+	bus = dbus.SystemBus()
+	bus_object = bus.get_object("org.freedesktop.ConsoleKit", "/org/freedesktop/ConsoleKit/Manager")
+	bus_object.Stop(dbus_interface="org.freedesktop.ConsoleKit.Manager")
+
+def Minutos_Segundos(tiempo):
+	int(tiempo)
+	tiempo = tiempo / 60
+	return tiempo
+	
+def Horas_Segundos(tiempo):
+	int(tiempo)
+	tiempo = tiempo / ( 60 * 60 )
+	return tiempo
+	
+def Mixto_Segundos(tiempo):
+	tiempo = tiempo.split(':')
+	return Hora_Segundos(tiempo[1]) + Minutos_Segundos(tiempo[2]) + init(tiempo[3])
+	
+
 
 if __name__ == "__main__":
 		gui()
